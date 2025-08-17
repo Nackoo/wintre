42d8e3666628454f6c70868add425c8dae0d5fb9
Nackoo <nacko9921@gmail.com>
2025-08-17 23:27:51 +0800

    Update nonsense.js

--- script/nonsense.js
+++ script/nonsense.js
@@ -1,28 +1,28 @@
-const bookmark = document.getElementById('bookmarkOverlay');
-const profile = document.getElementById('profileOverlay');
-const profilesub = document.getElementById('profileSubOverlay');
-const user = document.getElementById('userOverlay');
-const usersub = document.getElementById('userSubOverlay');
-const tag = document.getElementById('tagSubOverlay');
-const viewer = document.getElementById('tweetViewer');
-const tweet = document.getElementById('tweetOverlay');
-const retweet = document.getElementById('retweetOverlay');
-const notification = document.getElementById('notificationOverlay');
-const comment = document.getElementById('commentOverlay');
+export const bookmark = document.getElementById('bookmarkOverlay');
+export const profile = document.getElementById('profileOverlay');
+export const profilesub = document.getElementById('profileSubOverlay');
+export const user = document.getElementById('userOverlay');
+export const usersub = document.getElementById('userSubOverlay');
+export const tag = document.getElementById('tagSubOverlay');
+export const viewer = document.getElementById('tweetViewer');
+export const tweet = document.getElementById('tweetOverlay');
+export const retweet = document.getElementById('retweetOverlay');
+export const notification = document.getElementById('notificationOverlay');
+export const comment = document.getElementById('commentOverlay');
 
-const bookmarksvg = document.querySelector('.smallbar img[src="image/bookmark.svg"]');
-const homesvg = document.querySelector('.smallbar img[src="image/home.svg"]');
-const usersvg = document.querySelector('.smallbar img[src="image/user.svg"]');
-const searchsvg = document.querySelector('.smallbar img[src="image/search.svg"]');
-const settingssvg = document.querySelector('.smallbar img[src="image/settings.svg"]');
-const notifsvg = document.getElementById('notifsvg');
+export const bookmarksvg = document.querySelector('.smallbar img[src="image/bookmark.svg"]');
+export const homesvg = document.querySelector('.smallbar img[src="image/home.svg"]');
+export const usersvg = document.querySelector('.smallbar img[src="image/user.svg"]');
+export const searchsvg = document.querySelector('.smallbar img[src="image/search.svg"]');
+export const settingssvg = document.querySelector('.smallbar img[src="image/settings.svg"]');
+export const notifsvg = document.getElementById('notifsvg');
 
-const bookmarkfilled = document.querySelector('.smallbar img[src="image/bookmark-filled.svg"]');
-const homefilled = document.querySelector('.smallbar img[src="image/home-filled.svg"]');
-const userfilled = document.querySelector('.smallbar img[src="image/user-filled.svg"]');
-const searchfilled = document.querySelector('.smallbar img[src="image/search-filled.svg"]');
-const settingsfilled = document.querySelector('.smallbar img[src="image/settings-filled.svg"]');
-const notiffilled = document.querySelector('.smallbar img[src="image/notification-filled.svg"]');
+export const bookmarkfilled = document.querySelector('.smallbar img[src="image/bookmark-filled.svg"]');
+export const homefilled = document.querySelector('.smallbar img[src="image/home-filled.svg"]');
+export const userfilled = document.querySelector('.smallbar img[src="image/user-filled.svg"]');
+export const searchfilled = document.querySelector('.smallbar img[src="image/search-filled.svg"]');
+export const settingsfilled = document.querySelector('.smallbar img[src="image/settings-filled.svg"]');
+export const notiffilled = document.querySelector('.smallbar img[src="image/notification-filled.svg"]');
 
 const panelsToHide = () => [
   profile, profilesub, user, usersub, tag, document.getElementById("followOverlay"),
@@ -47,6 +47,14 @@ const outlineIcons = {
   notifsvg
 };
 
+const overlayMap = {
+  bookmarksvg: bookmark,     
+  usersvg: profile,
+  searchsvg: user,          
+  settingssvg: profilesub,
+  notifsvg: notification
+};
+
 const filledIconMap = {
   bookmarksvg: bookmarkfilled,
   homesvg: homefilled,
@@ -59,10 +67,11 @@ const filledIconMap = {
 const clickHandler = (clickedIcon) => {
   return () => {
     panelsToHide().forEach(p => p?.classList.add("hidden"));
-    filledIcons.forEach(icon => icon?.classList.add('hidden'));
-    Object.values(outlineIcons).forEach(icon => icon?.classList.remove('hidden'));
-    outlineIcons[clickedIcon]?.classList.add('hidden');
-    filledIconMap[clickedIcon]?.classList.remove('hidden');
+    filledIcons.forEach(icon => icon?.classList.add("hidden"));
+    Object.values(outlineIcons).forEach(icon => icon?.classList.remove("hidden"));
+    outlineIcons[clickedIcon]?.classList.add("hidden");
+    filledIconMap[clickedIcon]?.classList.remove("hidden");
+    overlayMap[clickedIcon]?.classList.remove("hidden");
   };
 };
 
@@ -117,6 +126,13 @@ function closeUser() {
   usersub?.classList.add('hidden');
   user.classList.remove('hidden');
 }
+
+window.hideprofile = hideprofile;
+window.hideuser = hideuser;
+window.hidebookmark = hidebookmark;
+window.hidesettings = hidesettings;
+window.hidenotif = hidenotif;
+window.closeUser = closeUser
 
 document.body.addEventListener("click", async (e) => {
   const userLink = e.target.closest(".user-link");
